b02ccea240da3cc596fe01a48588fd1fed7f75c4
Aaron Clark <aaronpclark92@gmail.com>
2020-02-12 18:04:46 -0600

    Day one MVP Complete

--- src/player.py
+++ src/player.py
@@ -1,2 +1,30 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    """Information for current player in game."""
+
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __str__(self):
+        return f'{self.name}, {self.current_room}'
+
+    def __repr__(self):
+        return f'{self.name}, {self.current_room}'
+
+    def print_current_room(self):
+        print(f'{self.name} is currently in {self.current_room}')
+
+    def move(self, direction):
+        # check if user can move in that direction
+        if getattr(self.current_room, f'{direction}_to'):
+            print(f'{self.name} fumbles from {self.current_room}...')
+            self.current_room = getattr(self.current_room, f'{direction}_to')
+            print(f'''...to...
+{self.current_room}, {self.current_room.description}
+            ''')
+        else:
+            print('Player cannot move that direction from here.')
--- src/room.py
+++ src/room.py
@@ -1,2 +1,20 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    """class for Rooms in adventure game"""
+    n_to = None
+    e_to = None
+    s_to = None
+    w_to = None
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f'{self.name}, {self.description}'
